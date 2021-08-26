52458b6def879f54b74459b89e5d68b9afbf6119
agahalinasir363@gmail.com <agahalinasir363@gmail.com>
2021-08-26 12:51:49 +0500

    auth saga

--- src/components/Patient/PatientHome.js
+++ src/components/Patient/PatientHome.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Tile from "../Tile";
+import {Redirect} from "react-router-dom";
+import logo from "../../assets/images/logo.png"
 
 const PatientHome = () => {
+
+    const onBookAppointmentClick =  () => {
+        alert('clicked');
+    }
+
     return(
         <>
-            <Tile/>
+            <div className="row">
+                <div className="col-sm-4 d-flex justify-content-center">
+                    <Tile imgPath={logo} text={"Book Appointment"} func={onBookAppointmentClick}/>
+                </div>
+                <div className="col-sm-4 d-flex justify-content-center">
+                    <Tile/>
+                </div>
+                <div className="col-sm-4 d-flex justify-content-center">
+                    <Tile/>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,31 +2,17 @@ import React from "react";
 import {Button, Card, Container, Form} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
-const Tile = () => {
+const Tile =(props) => {
     return(
         <>
-            <Container className= "w-auto float">
-                <Card>
+                <Card onClick={props.func}>
                     <Card.Body>
-                        <h2 className="text-center mb-4">Welcome Patient!</h2>
-                        <Form className="float-start">
-                            <Button className="mt-4 float-start"  type={"button"}>
-                                Sign Up
-                            </Button>
-                        </Form>
-                        <Form className="align-items-center">
-                            <Button className="mt-4" type={"button"}>
-                                Sign Up
-                            </Button>
-                        </Form>
-                        <Form className="float-end">
-                            <Button className="mt-4" type={"button"}>
-                                Sign Up
-                            </Button>
-                        </Form>
+                            <img src={props.imgPath}/>
                     </Card.Body>
+                    <Card.Footer className="d-flex justify-content-center">
+                        {props.text}
+                    </Card.Footer>
                 </Card>
-            </Container>
         </>
     )
 }
