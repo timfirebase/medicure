0ecb40ca9104476f694957d3788893f446a5aea2
agahalinasir363@gmail.com <agahalinasir363@gmail.com>
2021-08-27 12:55:03 +0500

    ui fixed

--- src/components/UI/Footer/Footer.js
+++ src/components/UI/Footer/Footer.js
@@ -3,13 +3,15 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 
 const Footer = () => {
     return (
-        <Navbar expand="lg" bg="primary" variant="dark" className="fixed-bottom">
-            <Container className="justify-content-center">
-                    <Nav>
-                        <Nav.Link href="#deets">Copyright 2021</Nav.Link>
-                    </Nav>
-            </Container>
-        </Navbar>
+        <div className="container pt-5">
+            <Navbar expand="lg" bg="primary" variant="dark" className="fixed-bottom mt-5">
+                <Container className="justify-content-center">
+                        <Nav>
+                            <Nav.Link href="#deets">Copyright 2021</Nav.Link>
+                        </Nav>
+                </Container>
+            </Navbar>
+        </div>
     );
 }
 
