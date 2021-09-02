4b691ceccbd9fea67cc190bd809a404cc8c9ed41
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-09-02 19:22:25 +0500

    manage doctors fixes

--- src/components/Admin/ManageDoctors.js
+++ src/components/Admin/ManageDoctors.js
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from "react";
 import ViewDoctors from "./ViewDoctors";
-import {Button, Card, Container, Form, Row} from "react-bootstrap";
 import Swal from "sweetalert2";
 import {useHistory} from "react-router-dom";
-import * as authActions from "../../store/actions/AuthActions";
 import {connect} from "react-redux";
-import * as PatientActions from "../../store/actions/PatientActions";
 import "react-multi-date-picker/styles/layouts/mobile.css";
 import DateTimePicker from "../UI/DateTimePicker/DateTimePicker";
 import * as DoctorActions from "../../store/actions/DoctorActions";
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import { makeStyles } from '@material-ui/core/styles';
+import MuiAccordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import { withStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 const ManageDoctors = (props) => {
 
@@ -68,60 +73,96 @@ const ManageDoctors = (props) => {
         Swal.fire('Doctor Deleted!','','success');
     }
 
+    const useStyles = makeStyles({
+        content: {
+            justifyContent: "center"
+        }
+    });
+
+    const classes = useStyles();
+
+    const Accordion = withStyles({
+        root: {
+            border: '2px solid rgba(0, 0, 0, .125)',
+            boxShadow: 'none',
+            '&:not(:last-child)': {
+                borderBottom: 0,
+            },
+            '&:before': {
+                display: 'none',
+            },
+            '&$expanded': {
+                margin: 'auto',
+            },
+        },
+        expanded: {},
+    })(MuiAccordion);
+
     return (
         <>
             <ViewDoctors heading="Manage Doctors" doctors={props.doctors} onDeleteClick={onDeleteClick}/>
-            <Container className= "w-auto">
+            <Container className= "w-50">
                 <Row className={"d-flex justify-content-center pt-3 pb-3"}>
-                    <Card className={"mt-4 mb-4"} style={{width:'50%', marginLeft:'1%'}}>
-                        <Card.Body className="p-3">
-                            <h2 className="text-center mb-4">Add a Doctor</h2>
-                            <Form autoComplete="off">
-                                <Form.Group id="name">
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control type="text" required value={name} onChange={(event)=>{setName(event.target.value)}}/>
-                                </Form.Group>
-                                <Form.Group id="email">
-                                    <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" required value={email} onChange={(event)=>{setEmail(event.target.value)}}/>
-                                </Form.Group>
-                                <Form.Group id="password">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password"  required value={password} onChange={(event)=>{setPassword(event.target.value)}}/>
-                                </Form.Group>
-                                <Form.Group id="phone">
-                                    <Form.Label>Contact No.</Form.Label>
-                                    <Form.Control type="number" required value={phone} onChange={(event)=>{setPhone(event.target.value)}}/>
-                                </Form.Group>
-                                <Form.Group id="availability">
-                                    <Form.Label>Availability</Form.Label>
-                                    <div>
-                                        <DateTimePicker availability={availability} setAvailability={setAvailability}/>
-                                    </div>
-                                </Form.Group>
-                                <Button className="w-100 mt-4" type={"submit"}
-                                        onClick={(event) => {
-                                            event.preventDefault();
-                                            availability.map(av => {
-                                                formattedDates.push(av.format("MMMM DD YYYY HH:mm A"));
-                                            });
-                                            Swal.fire({
-                                                title: 'Please wait...',
-                                                html: '',
-                                                allowEscapeKey: false,
-                                                allowOutsideClick: false,
-                                                didOpen: () => {
-                                                    Swal.showLoading()
-                                                }
-                                            })
-                                            props.onAddDoctor(doctor);
-                                            clearFormField();
-                                        }}>
-                                    Add
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
+                    <Accordion className={"justify-content-center border-primary"}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                            classes={{ content: classes.content }}
+                        >
+                            <span className="h2 text-center">Add a Doctor</span>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                                <Card className={"w-100"}>
+                                    <Card.Body>
+                                        <Form autoComplete="off">
+                                            <Form.Group id="name">
+                                                <Form.Label>Name</Form.Label>
+                                                <Form.Control type="text" required value={name} onChange={(event)=>{setName(event.target.value)}}/>
+                                            </Form.Group>
+                                            <Form.Group id="email">
+                                                <Form.Label>Email</Form.Label>
+                                                <Form.Control type="email" required value={email} onChange={(event)=>{setEmail(event.target.value)}}/>
+                                            </Form.Group>
+                                            <Form.Group id="password">
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control type="password"  required value={password} onChange={(event)=>{setPassword(event.target.value)}}/>
+                                            </Form.Group>
+                                            <Form.Group id="phone">
+                                                <Form.Label>Contact No.</Form.Label>
+                                                <Form.Control type="number" required value={phone} onChange={(event)=>{setPhone(event.target.value)}}/>
+                                            </Form.Group>
+                                            <Form.Group id="availability">
+                                                <Form.Label>Availability</Form.Label>
+                                                <div className="col-md-10">
+                                                    <DateTimePicker availability={availability} setAvailability={setAvailability}/>
+                                                </div>
+                                            </Form.Group>
+                                            <Button className="w-100 mt-4 btn-md" type={"submit"}
+                                                    onClick={(event) => {
+                                                        event.preventDefault();
+                                                        availability.map(av => {
+                                                            formattedDates.push(av.format("MMMM DD YYYY HH:mm A"));
+                                                        });
+                                                        Swal.fire({
+                                                            title: 'Please wait...',
+                                                            html: '',
+                                                            allowEscapeKey: false,
+                                                            allowOutsideClick: false,
+                                                            didOpen: () => {
+                                                                Swal.showLoading()
+                                                            }
+                                                        })
+                                                        props.onAddDoctor(doctor);
+                                                        clearFormField();
+                                                    }}>
+                                                Add
+                                            </Button>
+                                        </Form>
+                                    </Card.Body>
+                                </Card>
+                        </AccordionDetails>
+                    </Accordion>
                 </Row>
             </Container>
         </>
