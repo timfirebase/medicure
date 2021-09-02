8de9d3d80af851e5d4a5ff9f8b93fb86d545a2f4
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-09-03 00:53:52 +0500

    view appointments fixes

--- src/components/Admin/ViewAppointment.js
+++ src/components/Admin/ViewAppointment.js
@@ -1,73 +1,18 @@
 import React, {useEffect} from "react";
-import Grid from "../UI/Grid/Grid";
 import * as PatientActions from "../../store/actions/PatientActions";
 import {connect} from "react-redux";
-
-const columns = [
-    {
-        field: 'id',
-        headerName: 'Sr No.',
-        headerAlign: 'center',
-        width: 120
-    },
-    {
-        field: 'patientName',
-        headerName: 'Patient Name',
-        headerAlign: 'center',
-        width: 180
-    },
-    {
-        field: 'symptoms',
-        headerName: 'Symptoms',
-        headerAlign: 'center',
-        width: 200
-    },
-    {
-        field: 'doctorName',
-        headerName: 'Doctor Name',
-        headerAlign: 'center',
-        width: 200
-    },
-    {
-        field: 'availability',
-        headerName: 'Doctor Availability',
-        headerAlign: 'center',
-        width: 300
-    },
-    {
-        field: 'doctorEmail',
-        headerName: 'Doctor Email',
-        headerAlign: 'center',
-        width: 180
-    }
-];
+import AppointmentGrid from "../UI/Grid/AppointmentGrid";
 
 const ViewAppointment = (props) => {
 
     useEffect(()=>{
-        if(props.patient) {
+        if(props.admin) {
             props.getAllAppointments();
         }
     },[]);
-
-    let grid='';
-
-    if(props.allAppointments && props.allAppointments.length > 0) {
-        const rows = [];
-        props.allAppointments.map((appointment, index) => {
-            const row = {
-                id: index+1, patientName: appointment.name,symptoms: appointment.symptoms, doctorName: appointment.doctorName,
-                availability: appointment.availability,doctorEmail: appointment.doctorEmail
-            };
-            rows.push(row);
-        });
-        grid =  <Grid rows={rows} columns={columns} pageSize={parseInt("5")}/>;
-    }
-
     return (
         <>
-                <span className="h1 p-3 d-flex justify-content-center"> All Patient Appointments </span>
-                {grid}
+            <AppointmentGrid appointments={props.allAppointments} role={props.admin? props.admin.role : ""}/>
         </>
     );
 }
@@ -76,7 +21,7 @@ const ViewAppointment = (props) => {
 const mapStateToProps = state => {
     return {
         allAppointments : state.patientRdcr.allAppointments,
-        patient: state.authRdcr.user
+        admin: state.authRdcr.user
     };
 };
 
--- src/components/Patient/PatientAppointments.js
+++ src/components/Patient/PatientAppointments.js
@@ -1,46 +1,7 @@
 import React, {useEffect} from "react";
-import Grid from "../UI/Grid/Grid";
 import * as PatientActions from "../../store/actions/PatientActions";
 import {connect} from "react-redux";
-
-const columns = [
-    {
-        field: 'id',
-        headerName: 'Sr No.',
-        headerAlign: 'center',
-        width: 120
-    },
-    {
-        field: 'patientName',
-        headerName: 'Patient Name',
-        headerAlign: 'center',
-        width: 180
-    },
-    {
-        field: 'symptoms',
-        headerName: 'Symptoms',
-        headerAlign: 'center',
-        width: 200
-    },
-    {
-        field: 'doctorName',
-        headerName: 'Doctor Name',
-        headerAlign: 'center',
-        width: 200
-    },
-    {
-        field: 'availability',
-        headerName: 'Doctor Availability',
-        headerAlign: 'center',
-        width: 300
-    },
-    {
-        field: 'doctorEmail',
-        headerName: 'Doctor Email',
-        headerAlign: 'center',
-        width: 180
-    }
-];
+import AppointmentGrid from "../UI/Grid/AppointmentGrid";
 
 const PatientAppointments = (props) => {
 
@@ -49,26 +10,9 @@ const PatientAppointments = (props) => {
             props.getAppointmentsByPatientId(props.patient.id);
         }
     },[]);
-
-
-    let grid = '';
-
-    if(props.appointments) {
-        const rows = [];
-        props.appointments.map((appointment, index) => {
-            const row = {
-                id: index+1, patientName: appointment.name,symptoms: appointment.symptoms, doctorName: appointment.doctorName,
-                availability: appointment.availability,doctorEmail: appointment.doctorEmail
-            };
-            rows.push(row);
-        });
-        grid =  <Grid rows={rows} columns={columns} pageSize={parseInt("5")}/>;
-    }
-
     return (
         <>
-        <span className="h1 p-3 d-flex justify-content-center"> Appointments </span>
-            {grid}
+            <AppointmentGrid appointments={props.appointments} role={props.patient? props.patient.role : ""}/>
         </>
     );
 }
